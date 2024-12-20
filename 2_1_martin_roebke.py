40c56aa1ebe11e0ccd0b013d6d5244b22d473de5
Martin Röbke <martin.roebke@zeiss.com>
2024-12-20 11:43:51 +0100

    Update 2_1_martin_roebke.py
    - Add missing `r` raw specifier to labels.
    
    - add typing
    
    - remove `from __future__ import ` needed for python 2.
    
    - format file

--- 2_1_martin_roebke.py
+++ 2_1_martin_roebke.py
@@ -10,17 +10,17 @@ Drei Methoden:
    AE(x) = 1/3h( 8 (f(x+h/4)-f(x-h/4)) - (f(x+h/2)-f(x-h/2)))
 """
 
-from __future__ import division, print_function     # problemlose Division
-import numpy as np                                  # Arrays, Mathe etc
-import matplotlib.pyplot as plt                     # Plotten
+from typing import Callable
+import numpy as np  # Arrays, Mathe etc
+import matplotlib.pyplot as plt  # Plotten
 
 
-def relativ_Fehler(wert, erwartet):
+def relativ_Fehler(wert: float, erwartet: float):
     """Relativer Fehler von 'wert' bezueglich einer Referenz 'erwartet'."""
     return abs((wert - erwartet) / erwartet)
 
 
-def func(x=0, abl=0, string=False):
+def func(x: float = 0, abl: int = 0, string: bool = False):
     """Reelle Funktion mit Argument x, Grundlage zu Berechnung.
     abl:
         0 Funktionswert.
@@ -29,36 +29,36 @@ def func(x=0, abl=0, string=False):
     """
     if string:
         # gibt Beschreibung f(x) als String zurueck
-        return "arctan(x^3)"
+        return r"$arctan(x^3)$"
     if abl == 0:
         # Funktion f(x)
         return np.arctan(x**3)
     if abl == 1:
         # Erste analytische Ableitung der Funktion f(x)
-        return 3.*x*x / (x**6 + 1)
+        return 3.0 * x * x / (x**6 + 1)
 
     raise ValueError("'func' konnte Parameter nicht erkennen.")
 
 
-def vorwaertsDiff(f, x, h):
-    """return Ableitung der Funktion f an Stelle x
-    nach Vorwaertsdifferenz mit Parameter h
-    """
-    return 1.0/h * (f(x+h) - f(x))
+def vorwaertsDiff(f: Callable[[float], float], x: float, h: np.ndarray[float]):
+    """return Ableitung der Funktion f an Stelle x nach Vorwaertsdifferenz mit Parameter h"""
+    return 1.0 / h * (f(x + h) - f(x))
 
 
-def zentralDiff(f, x, h):
-    """return Ableitung der Funktion f an Stelle x
-    nach Zentraldifferenz mit Parameter h
-    """
-    return 1.0/h * (f(x+h/2) - f(x-h/2))
+def zentralDiff(f: Callable[[float], float], x: float, h: np.ndarray[float]):
+    """return Ableitung der Funktion f an Stelle x nach Zentraldifferenz mit Parameter h"""
+    return 1.0 / h * (f(x + h / 2) - f(x - h / 2))
 
 
-def extrapolDiff(f, x, h):
+def extrapolDiff(f: Callable[[float], float], x: float, h: np.ndarray[float]):
     """return Ableitung der Funktion f an Stelle x
     nach '1/3h *( 8*(f(x + h/4)-f(x - h/4)) - (f(x + h/2) - f(x - h/2)))'
     """
-    return 1/(3*h) * (8*(f(x+h/4) - f(x-h/4)) - (f(x+h/2) - f(x-h/2)))
+    return (
+        1
+        / (3 * h)
+        * (8 * (f(x + h / 4) - f(x - h / 4)) - (f(x + h / 2) - f(x - h / 2)))
+    )
 
 
 def main():
@@ -66,9 +66,9 @@ def main():
     Zur Eingabe der Parameter und Darstellung der Berechnung.
     """
     # Parameter
-    f = func                                        # genutzte Funktion
-    auswertung_x = 1/3.                             # x-Position
-    h = 10.0 ** np.linspace(-10, 0, 100)            # Array h Log-10
+    f = func  # genutzte Funktion
+    auswertung_x: float = 1 / 3  # x-Position
+    h: np.ndarray[float] = 10.0 ** np.linspace(-10, 0, 100)  # Array h Log-10
 
     # Anfangstext Konsole
     print(__doc__)
@@ -89,18 +89,18 @@ def main():
     relFehlerZD = relativ_Fehler(ZentralDiff, ableitung_wert)
     relFehlerED = relativ_Fehler(ExtrapolDiff, ableitung_wert)
 
-    fig = plt.figure(figsize=(12, 10))               # Figure
-    fig.subplots_adjust(0.08, 0.07, 0.95, 0.91)     # Begrenzung [l,b,r,t]
+    fig = plt.figure(figsize=(12, 10))  # Figure
+    fig.subplots_adjust(0.08, 0.07, 0.95, 0.91)  # Begrenzung [l,b,r,t]
 
     # Erstelle Subplot Zeichenfenster
     plt.subplot(111, xscale="log", yscale="log")
     stitle = "Numerische Ableitung f(x)=" + f(string=True)
     # Plot-Titel
-    plt.suptitle(stitle, fontsize='x-large')
+    plt.suptitle(stitle, fontsize="x-large")
     plt.title("Doppeltlogaritmisch")
     # Achsenbeschriftung
-    plt.xlabel("Parameter h", fontsize='large')
-    plt.ylabel("Relativer Fehler", fontsize='large')
+    plt.xlabel("Parameter h", fontsize="large")
+    plt.ylabel("Relativer Fehler", fontsize="large")
 
     plt.plot(h, relFehlerVD, "bo", label="Vorwaertsdifferenz", ms=4.5, mew=0)
     plt.plot(h, relFehlerZD, "go", label="Zentraldifferenz", ms=4.5, mew=0)
@@ -109,25 +109,24 @@ def main():
     # Erwartetes Skalierungsverhalten h^alpha
     # Linear
     h_l = h[6:].copy()
-    plt.plot(h_l, h_l * 3.54*2, "b", label="$h^{\ 1.0}$", lw=1)
+    plt.plot(h_l, h_l * 3.54 * 2, "b", label=r"$h^{\ 1}$", lw=1)
     # Quadratisch
     h_sq = h[37:].copy()
-    plt.plot(h_sq, h_sq*h_sq * 0.66*2, "g", label="$h^{\ 2.0}$", lw=1)
+    plt.plot(h_sq, h_sq * h_sq * 0.66 * 2, "g", label=r"$h^{\ 2}$", lw=1)
     # h^4
     h_quad = h[65:].copy()
-    plt.plot(h_quad, h_quad**4.0 * 0.032*2, "r", label="$h^{\ 4.0}$", lw=1)
+    plt.plot(h_quad, h_quad**4.0 * 0.032 * 2, "r", label=r"$h^{\ 4}$", lw=1)
     # Rundung invers zu h
-    plt.plot(h, (h**-1)*(10**-17)*3, "k", label="$h^{- 1.0}$", lw=3)
+    plt.plot(h, (h**-1) * (10**-17) * 3, "k", label=r"$h^{- 1}$", lw=3)
 
-    # Erschaffe Graphen-Legende
-    legend = plt.legend(loc='upper left', shadow=True)
+    legend = plt.legend(loc="upper left", shadow=True)
 
-    plt.show()                                      # Zeigen des Fensters
+    plt.show()  # Zeigen des Fensters
 
 
 # -------------Main Programm---------------
 if __name__ == "__main__":
-    main()                                          # Rufe Mainroutine
+    main()
 
 """Kommentar:
 Beispiel der Tiefe Gleitkomma-Diskretisierung, Mantisse 52 bit:
